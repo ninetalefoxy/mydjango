d51bf7c0ecb82cbea2c329ff74a3868e3faed405
jagtap <suraj100jagtap@gmail.com>
2021-04-13 12:06:59 +0530

    added models and registered to admin

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # add in thumbnail later
+    # add in author later
+
+    def __str__(self):
+        return self.title
+
+    
+
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+def article_list(request):
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/index_article.html',{'articles':articles})
+
+
